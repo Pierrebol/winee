515d3f05c91f6a789830ed1b510c8878327053d0
Thomas Dubet <thomas.dubet@edhec.com>
2021-06-22 16:12:06 +0200

    adding the through in the model delivery, order and user

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,4 +1,5 @@
 class Delivery < ApplicationRecord
   belongs_to :user
   has_many :delivery_products
+  has_many :wines, through: :order_products
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_wines
+  has_many :wines, through: :order_wines
+
 end
