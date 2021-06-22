c1060f7bbceb663dda7b2c78cc5744f59b67b6bb
Thomas Dubet <thomas.dubet@edhec.com>
2021-06-22 14:30:29 +0200

    validation

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,4 @@
 class Delivery < ApplicationRecord
   belongs_to :user
+  has_many :delivery_products
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :photo
+  has_many :orders
+  has_many :cellars
+  has_many :cellars
+  has_many :reviews
+
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "Veuillez utilisez un format correct" }
 end
