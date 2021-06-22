be550d0d285d410b4062ddb25846ced3beee4922
pierrebollack <pierre.bollack@edhec.com>
2021-06-22 18:16:07 +0200

    add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :wines do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create, :update, :edit]
   end
 
   get '/my_cellar', to: 'wines#user_index'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/advices', to: 'pages#advices'
+
+  resources :orders, only: [:create]
 end
