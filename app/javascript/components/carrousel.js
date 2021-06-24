c6b6391e24487279b32c2a5d122e579a0a2b6ee7
pierrebollack <pierre.bollack@edhec.com>
2021-06-24 11:00:20 +0200

    paulo

---
+++ app/javascript/components/carrousel.js
@@ -0,0 +1,10 @@
+const initCarrousel = () => {
+  $('.carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+  });
+};
+
+export { initCarrousel };
