e47e0b199f65dcae5c850dc14db130d4c140c13c
Thomas Dubet <thomas.dubet@edhec.com>
2021-06-25 10:51:35 +0200

    adding  the  modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
+	// This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -35,3 +35,13 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   // initCarrousel();
 });
+
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "This is a great alert, isn't it?",
+  icon: "success"
+});
+	
