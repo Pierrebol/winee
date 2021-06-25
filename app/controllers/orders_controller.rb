b11641fb73d52ffe9e0c82e7e5ce179e6167ce5c
pierrebollack <pierre.bollack@edhec.com>
2021-06-25 12:24:00 +0200

    add cart logo to navbar

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  def show
+    @orders = Order.find(params[:id])
+  end
+
+  def user_index
+    @orders = Winebox.where(user: current_user)
+  end
+
+  def create
+  end
+
+  def destroy
+    # "uniquement si status == cart"
+  end
 end
