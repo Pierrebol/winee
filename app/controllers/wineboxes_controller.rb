c5b3827cd4174f440da72206063ac21ac7c4d2aa
Thomas Dubet <thomas.dubet@edhec.com>
2021-06-28 10:15:35 +0200

    adding the update feature on stocker une bouteille

--- app/controllers/wineboxes_controller.rb
+++ app/controllers/wineboxes_controller.rb
@@ -1,21 +1,24 @@
 class WineboxesController < ApplicationController
-
-	def create
-	@winebox = Winebox.new(winebox_params)
+def create
+  if Winebox.find_by(wine_id: params[:winebox][:wine_id], user_id: current_user).nil?
+    @winebox = Winebox.new(winebox_params)
 	@winebox.user = current_user
-	    if @winebox.save!
-	      redirect_to my_cellar_path, notice: "#{@winebox.quantity_of_wine} bouteilles de \"#{@winebox.wine.name}\" ajoutées"
-	    else
-	      redirect_to my_cellar_path
-		end
+    if @winebox.save!
+      redirect_to my_cellar_path, notice: "#{@winebox.quantity_of_wine} bouteilles de \"#{@winebox.wine.name}\" ajoutées"
+    else
+      redirect_to my_cellar_path
 	end
-
-
+  else
+  	@winebox = (Winebox.find_by(wine_id: params[:winebox][:wine_id], user_id: current_user))
+    @winebox.quantity_of_wine += (params[:winebox][:quantity_of_wine].to_i)
+    @winebox.save!
+    redirect_to my_cellar_path(@winebox), notice: "Le nombre de bouteilles de \"#{@winebox.wine.name}\" a été mis à jour"
+  end
+end
 
   private
 
   def winebox_params
     params.require(:winebox).permit(:wine_id, :quantity_of_wine)
   end  
-
 end
