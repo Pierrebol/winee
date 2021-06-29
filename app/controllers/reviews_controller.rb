5af934d4bad18fa4efedfd18d41908e3b53f958e
Thomas Dubet <thomas.dubet@edhec.com>
2021-06-29 15:51:00 +0200

    changing the controller and the simple fomr but still bugging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,13 +7,19 @@ class ReviewsController < ApplicationController
 
 	def create
 	    @review = Review.new(review_params)
-	    @restaurant = Restaurant.find(params[:restaurant_id])
-	    @review.restaurant = @restaurant
+	    @wine = Wine.find(params[:wine_id])
+	    @review.wine = @wine
 	    if @review.save
-	      redirect_to restaurant_path(@restaurant)
+	      redirect_to wine_path(@wine)
 	    else
-	      render :new
+	      render :show
 	    end
   	end
   	
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
